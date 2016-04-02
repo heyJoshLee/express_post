0b0a632dd1d9fdad880fc8e4d97e83350474d550
jleewebdev <lee45276@gmail.com>
2016-04-02 19:16:30 +0800

    Add user pages, post edit and delete buttons only show if author is logged in

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../models/user.js");
+var Post = require("../models/post.js");
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find({}, function(err, doc) {
+      if(err) {
+        console.log(err);
+        return
+      } else {
+        res.render("users_index", {
+        title: "All users",
+        users: doc
+      });
+      }
+  });
+
+});
+
+router.get("/:username", function(req, res, next) {
+  var author = req.params["username"]
+  Post.find({ author: author}, function(err, doc) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.render("users_show", {
+        title: "User | " + author,
+        preview_user: author,
+        posts: doc
+      });
+    }
+  });
 });
 
 module.exports = router;
